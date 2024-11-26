87c638a576f9534dba734ed36e5c0cd215b10e9a
John Carlos <carlosjohn1630@gmail.com>
2024-11-27 06:28:45 +0800

    added a sitemap and robottxt

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.roleselector.xyz', // Replace with your domain
+    generateRobotsTxt: true, // Automatically generate robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
+  module.exports = config;
+  
